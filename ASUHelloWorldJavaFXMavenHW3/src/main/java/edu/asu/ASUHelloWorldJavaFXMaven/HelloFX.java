0d5f0f81ea615b5886d1cf81f965e5e93dff5680
anushkakg <anushkakgupta@gmail.com>
2024-09-24 16:04:38 -0700

    Changes button text and title

--- ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
+++ ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
@@ -14,9 +14,9 @@ public class HelloFX extends Application {
     public void start(Stage primaryStage) {
     	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
-        primaryStage.setTitle("ASU Hello World Fall 2024");
+        primaryStage.setTitle("CSE360 In Class Activity");
         Button btn = new Button();
-        btn.setText("Display: 'ASU says: Hello World!'");
+        btn.setText("Anushka's Second Commit'");
         btn.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
                 System.out.println("ASU says: Hello World!");
